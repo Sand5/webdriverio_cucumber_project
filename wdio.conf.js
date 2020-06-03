4c9c869973137c39dd3cd97243a4a12fe8a38258
Sanderson Imbeah <Sanderson_imbeah5@hotmail.com>
2020-06-03 17:31:49 +0100

    further change made

--- wdio.conf.js
+++ wdio.conf.js
@@ -79,7 +79,7 @@ exports.config = {
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
       // excludeDriverLogs: ['bugreport', 'server'],
 
-      'goog:chromeOptions': { args: ['headless'] }
+      //'goog:chromeOptions': { args: ['headless'] }
     },
   ],
   //
