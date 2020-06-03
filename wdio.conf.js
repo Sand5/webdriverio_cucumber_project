ef55c7737cc3614ce0641642579e893a058177d0
Sanderson Imbeah <Sanderson_imbeah5@hotmail.com>
2020-06-03 16:27:45 +0100

    changes to the wdio file config to support headless mode for chrome to work in travis ci

--- wdio.conf.js
+++ wdio.conf.js
@@ -78,7 +78,7 @@ exports.config = {
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
       // excludeDriverLogs: ['bugreport', 'server'],
 
-      //'goog:chromeOptions': { args: ['headless', 'disable-gpu'] }
+      'goog:chromeOptions': { args: ['headless', 'disable-gpu'] }
     },
   ],
   //
