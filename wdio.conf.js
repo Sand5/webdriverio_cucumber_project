dda82690a596026e0835340fc3c8f1b787048302
Sanderson Imbeah <Sanderson_imbeah5@hotmail.com>
2020-06-05 17:01:35 +0100

    small change

--- wdio.conf.js
+++ wdio.conf.js
@@ -173,7 +173,7 @@ exports.config = {
     source: true, // <boolean> hide source uris
     profile: [], // <string[]> (name) specify the profile to use
     strict: false, // <boolean> fail if there are any undefined or pending steps
-    tagExpression: process.env.CI ?`@regression`:`@regression or ${process.env.ENVIRONMENT} or @int`}, // <string> (expression) only execute the features or scenarios with tags matching the expression
+    tagExpression: [process.env.CI ?`@regression`:`@regression or ${process.env.ENVIRONMENT} or @int`], // <string> (expression) only execute the features or scenarios with tags matching the expression
     timeout: 60000, // <number> timeout for step definitions
     ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
   },
