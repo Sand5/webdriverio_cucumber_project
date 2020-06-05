add3c69af02baa9f6f676b11ab2f2608f5394a44
Sanderson Imbeah <Sanderson_imbeah5@hotmail.com>
2020-06-05 16:55:48 +0100

    file update

--- support/asserstions/verifyLinksContain.js
+++ support/asserstions/verifyLinksContain.js
@@ -1,6 +1,5 @@
 import searchResultsPage from "../../pages/SearchResults";
 
-
 /**
  * Ensure link text includes keyword
  *
@@ -8,13 +7,13 @@ import searchResultsPage from "../../pages/SearchResults";
  * @param {String} keyword Search keyword
  */
 export default (keyword) => {
-    searchResultsPage.searchResultsLinks.forEach(link => {
-      const linkText = link.getText().toLowerCase();
-      if (linkText) {
-        assert(
-          linkText.includes(keyword),
-          `Link ${linkText} does not include ${keyword}`
-        );
-      }
-    });
-  }
+  searchResultsPage.searchResultsLinks.forEach((link) => {
+    const linkText = link.getText().toLowerCase();
+    if (linkText) {
+      assert(
+        linkText.includes(keyword),
+        `Link ${linkText} does not include ${keyword}`
+      );
+    }
+  });
+};
