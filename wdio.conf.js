cca934424d0feeaf947af8a221415b3b30d30195
Sanderson <sanderson_imbeah5@hotmail.com>
2020-10-12 15:02:50 +0100

    changes to config

--- wdio.conf.js
+++ wdio.conf.js
@@ -86,7 +86,7 @@ exports.config = {
       // excludeDriverLogs: ['bugreport', 'server'],
 
       'goog:chromeOptions': { 
-        args: process.env.CI ? ['headless','disable-gpu'] :[],
+        args: process.env.CI ? ['headless'] :[],
     
       }
       
